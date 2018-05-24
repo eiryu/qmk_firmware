3bf766600595a9f5368c602713ed6adaeffbdf8a
eiryu <eiryu.co@gmail.com>
2018-05-25 02:53:25 +0900

    スペースキー長押しを⌘キーとしてみる

--- keyboards/planck/keymaps/eiryu/keymap.c
+++ keyboards/planck/keymaps/eiryu/keymap.c
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |Shift |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite| Ctrl | Alt  | GUI  |EISUU |    Space    |KANA  | Left | Down |  Up  |Right |
- * |      |      |      |      |Lower |             |Raise |      |      |      |      |
+ * |      |      |      |      |Lower |    GUI      |Raise |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,    KC_Y,    KC_U,                 KC_I,    KC_O,    KC_P,                  KC_BSPC},
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                 KC_G,    KC_H,    KC_J,                 KC_K,    KC_L,    KC_SCLN,               KC_QUOT},
-  {KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,    KC_N,    KC_M,                 KC_COMM, KC_DOT,  MT(MOD_RSFT, KC_SLSH), KC_ENT },
-  {BACKLIT,              KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_LANG2), KC_SPC,  KC_SPC,  LT(_RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,                 KC_RGHT}
+  {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,                  KC_Y,                  KC_U,                 KC_I,    KC_O,    KC_P,                  KC_BSPC},
+  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                 KC_G,                  KC_H,                  KC_J,                 KC_K,    KC_L,    KC_SCLN,               KC_QUOT},
+  {KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,                  KC_N,                  KC_M,                 KC_COMM, KC_DOT,  MT(MOD_RSFT, KC_SLSH), KC_ENT },
+  {BACKLIT,              KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_LANG2), MT(MOD_RGUI, KC_SPC),  MT(MOD_RGUI, KC_SPC),  LT(_RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,                 KC_RGHT}
 },
 
 /* Colemak
