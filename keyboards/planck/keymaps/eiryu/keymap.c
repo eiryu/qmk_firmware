f7ffbb9ab395ebca443cba71a8764658ffee3ed9
eiryu <eiryu.co@gmail.com>
2018-05-26 21:30:59 +0900

    Ctrl、Altを右にずらす

--- keyboards/planck/keymaps/eiryu/keymap.c
+++ keyboards/planck/keymaps/eiryu/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |      |      |      |      |      |      |      |      |      |      |Shift |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  | GUI  |Space |Space | GUI  | Left | Down |  Up  |Right |
+ * | Brite| Ctrl | Ctrl | Alt  | GUI  |Space |Space | GUI  | Left | Down |  Up  |Right |
  * |      |      |      |      |      |Lower |Raise |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,                  KC_BSPC},
   {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN,               KC_QUOT},
   {KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,    KC_COMM, KC_DOT,  MT(MOD_RSFT, KC_SLSH), KC_ENT },
-  {BACKLIT,              KC_LCTL, KC_LALT, KC_LGUI, KC_LGUI, LT(LOWER, KC_SPC),  LT(RAISE, KC_SPC),  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,                 KC_RGHT}
+  {BACKLIT,              KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_SPC),  LT(RAISE, KC_SPC),  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,                 KC_RGHT}
 },
 
 /* Colemak
