cf487d363ce3ac10e30da460135961a98a8a925e
eiryu <eiryu.co@gmail.com>
2018-12-23 14:57:33 +0900

    バックライト関連のキーをAJUSTに移動

--- keyboards/planck/keymaps/eiryu/keymap.c
+++ keyboards/planck/keymaps/eiryu/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,         KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,            KC_MINS},
   {LCTL_T(KC_ESC), KC_A,       KC_S,    KC_D,    KC_F,    KC_G,               KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN,         RCTL_T(KC_QUOT)},
   {KC_LSFT,        KC_Z,       KC_X,    KC_C,    KC_V,    KC_B,               KC_N,               KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_BSPC},
-  {BACKLIT,        KC_MS_BTN2, KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_SPC),  LT(RAISE, KC_ENT),  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT}
+  {KC_DEL,         KC_MS_BTN2, KC_LCTL, KC_LALT, KC_LGUI, LT(LOWER, KC_SPC),  LT(RAISE, KC_ENT),  KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT}
 },
 
 /* Colemak
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI,  RGB_VAD, KC_DEL },
   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, PLOVER,  _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______,  _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______}
+  {BACKLIT, BL_BRTG, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______}
 }
 
 
